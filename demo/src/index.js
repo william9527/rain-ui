43778baa55f5f4282c2582c28980bc7170ed5e1c
William <william@rayaegis.com>
2018-05-31 17:51:47 +0800

    Add first demo ui

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,12 +2,12 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 
 import Example from '../../src'
-
+import LoadingButton from '../../src/LoadingButton'
 class Demo extends Component {
   render() {
     return <div>
-      <h1>rain-ui Demo</h1>
-      <Example/>
+      <h1>my-component Demo</h1>
+      <LoadingButton/>
     </div>
   }
 }
---
+++ src/LoadingButton/LoadingButton.js
@@ -0,0 +1,11 @@
+import t from 'prop-types'
+import React, {Component} from 'react'
+
+class LoadingButton extends Component {
+  
+  render() {
+    return (<div>test1234</div>)
+  }
+}
+
+export default LoadingButton
